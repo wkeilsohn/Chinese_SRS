078bd2fbc288ab3454db02b0f4397a31df3b5eb0
William Keilsohn <kingwilliam@gmaail.com>
2025-09-11 14:50:00 +0900

    Updated Hanzi Reader

--- hanzi.py
+++ hanzi.py
@@ -4,6 +4,17 @@
 # Import Packages
 import os
 
+# Define variables
+cpath = os.getcwd()
+hpath = os.path.join(cpath, "Hanzi")
+
+HSK_1_file = os.path.join(hpath, "HV1.csv")
+HSK_2_file = os.path.join(hpath, "HV2.csv")
+HSK_3_file = os.path.join(hpath, "HV3.csv")
+HSK_4_file = os.path.join(hpath, "HV4.csv")
+HSK_5_file = os.path.join(hpath, "HV5.csv")
+HSK_6_file = os.path.join(hpath, "HV6.csv")
+
 # Define Functions
 
 # Run application
