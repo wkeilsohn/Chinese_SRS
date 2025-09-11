eade97d14f724b7350e15467a70e29e902d9835a
William Keilsohn <kingwilliam@gmaail.com>
2025-09-11 14:31:37 +0900

    First Commit

---
+++ hanzi.py
@@ -0,0 +1,11 @@
+# William Keilsohn
+# September 11 2025
+
+# Import Packages
+import os
+
+# Define Functions
+
+# Run application
+if __name__=="__main__":
+    print("Hello World")
---
+++ main.py
@@ -0,0 +1,16 @@
+# William Keilsohn
+# September 11 2025
+
+# Import Packages
+import numpy as np
+import pandas as pd
+import os
+
+## Import Custom Scripts
+
+
+# Define Global Variables
+
+# Define Functions
+
+# Run Application
