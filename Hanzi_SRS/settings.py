be854f1f39fb77c9091dc3ae016ea190bada8999
wkeilsohn <kingwilliam42@gmail.com>
2025-10-10 13:22:26 +0900

    Basic CSS

--- Hanzi_SRS/settings.py
+++ Hanzi_SRS/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For prod
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
